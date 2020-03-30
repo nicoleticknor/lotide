4887e199517f14c36854d715d87712e314cf92d5
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-03-30 23:49:01 +0000

    Implement Tail function for Lotide Challenge

---
+++ tail.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if ((actual === expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words));
+assertEqual(words.length, 3);
+
+console.log(tail([]));
+console.log(tail(["Han Solo"]));
