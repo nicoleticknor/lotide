edcb5eb50a8d3cb42150a4e578bb44a5d61ac60f
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-02 04:04:36 +0000

    assertObjectsEqual function completed

---
+++ assertObjectsEqual.js
@@ -0,0 +1,56 @@
+const eqArrays = function(firstArray, secondArray) {
+  if (!Array.isArray(firstArray) || !Array.isArray(secondArray)) {
+    return false;
+  }
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  let keysArray1 = Object.keys(object1);
+  let keysArray2 = Object.keys(object2);
+  if (keysArray1.length !== keysArray2.length) {
+    return false;
+  }
+  for (let key1 of keysArray1) {
+    if (Array.isArray(object1[key1])) {
+      if (eqArrays(object1[key1], object2[key1]) === false) {
+        return false;
+      }
+    } else if (object1[key1] !== object2[key1]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: [${inspect(actual)}] === [${inspect(expected)}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${inspect(actual)}] !== [${inspect(expected)}]`);
+  }
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+
+assertObjectsEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(eqObjects(ab, abc), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(eqObjects(cd, cd2), false);
