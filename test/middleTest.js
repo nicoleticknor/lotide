f8ec63608088812312f8478bd6c6ed80eda140e3
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-06 20:31:21 +0000

    test files for middle and tail added

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,35 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const chai = require('chai');
+const assert = chai.assert;
 
-assertArraysEqual(middle([1, 2, 3,]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, undefined, 3, 4]), [undefined, 3]);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe('#middle', () => {
+  it('should return [2] for [1, 2, 3]', () => {
+    assert(middle([1, 2, 3]), [2]);
+  });
+  //I don't understand how this uses assertDeepEqual
+
+  it('should return [2, 3] for [1, 2, 3, 4]', () => {
+    assert(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it('should return [undefined, 3] for [1, 2, 3, 4]', () => {
+    assert(middle([1, undefined, 3, 4]), [undefined, 3]);
+  });
+
+
+  it('should return [] for []', () => {
+    assert(middle([]), []);
+  });
+
+  it('should return [] for [1, 2]', () => {
+    assert(middle([1, 2]), []);
+  });
+
+});
+
+// assertArraysEqual(middle([1, 2, 3,]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, undefined, 3, 4]), [undefined, 3]);
+// assertArraysEqual(middle([]), []);
+// assertArraysEqual(middle([1, 2]), []);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,22 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const chai = require('chai');
+const assert = chai.assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const expected = "Lighthouse,Labs";
-console.log(assertEqual(tail(words), expected));
+describe('#tail', () => {
+  it('should return "Lighthouse,Labs" for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), "Lighthouse,Labs");
+  });
+  it('should return "" for ["Yo Yo"]', () => {
+    assert.strictEqual(tail(["Yo Yo"]), '');
+  });
+
+  it('should return "" for []', () => {
+    assert.strictEqual(tail([]), '');
+  });
+
+  it('should return "3" for [1, 3]', () => {
+    assert.strictEqual(tail([1, 3]), '3');
+  });
+
+});
