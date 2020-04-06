980a344a8faf72b58cf79565de1d1eafbcd87ce1
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-06 20:05:49 +0000

    installed mocha and chai. adding gitignore, package.json, and node_modules for mocha/chai. refactored test/headTest.js for use with mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,28 @@
 const assertEqual = require('../assertEqual');
+const chai = require('chai');
+const assert = chai.assert;
 
 const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([2,6,7]), 5);
-assertEqual(head(["Goodbye", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head(["Goodbye"]), "Hello");
-assertEqual(head(["Hello"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3,]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it('returns undefined for []', () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+});
