e9a33876422ce93e54a903415954004ccf561e89
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-06 20:32:13 +0000

    edit middleTest.js to remove old code commented out

--- test/middleTest.js
+++ test/middleTest.js
@@ -27,9 +27,3 @@ describe('#middle', () => {
   });
 
 });
-
-// assertArraysEqual(middle([1, 2, 3,]), [2]);
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-// assertArraysEqual(middle([1, undefined, 3, 4]), [undefined, 3]);
-// assertArraysEqual(middle([]), []);
-// assertArraysEqual(middle([1, 2]), []);
